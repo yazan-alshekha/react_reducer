0bd57ac15bb6958af8e08069be59c8964f3c367f
yazan alshekha <yazan.alshekha@outlook.com>
2022-08-09 17:31:22 +0300

    simple reducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState , useReducer} from "react"
+
+const ACTIONS={
+  INCREMENT: "increment",
+  DECREMENT:"decrement"
+}
+
+
+function reducer( state,action ){
+  switch (action.type){
+    
+    case ACTIONS.INCREMENT:
+      return {count : state.count + 1} ; 
+
+    case ACTIONS.DECREMENT:
+      return {count :state.count-1};  
+      
+      default :
+        return state; 
+    }
+
+}
+
 
 function App() {
+
+
+const [state , dispatch]= useReducer(reducer , { count : 0 } );
+  // const [count,setCount]=useState(0);
+
+  function increment(){
+    // setCount(count+1);
+    // setCount(i => i+1);
+    dispatch({type: ACTIONS.INCREMENT});
+
+  }
+
+  function decrement(){
+    // setCount(i => i+1);
+    dispatch({type:ACTIONS.DECREMENT});
+
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button onClick={decrement}>-</button>
+    <span>{state.count}</span>
+    <button onClick={increment}>+</button>
+    </>
   );
 }
 
